ff5cd4085a6aaf39a63b12398f6013cec4e74357
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-04-30 18:49:34 +0800

    Initialize elixir project

---
+++ lib/jvc.ex
@@ -0,0 +1,18 @@
+defmodule Jvc do
+  @moduledoc """
+  Documentation for Jvc.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Jvc.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/jvc_test.exs
@@ -0,0 +1,8 @@
+defmodule JvcTest do
+  use ExUnit.Case
+  doctest Jvc
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
