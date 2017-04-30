926c27b333e35ecf084be31d9306879e5e1bb598
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-04-30 18:57:06 +0800

    Add initial test cli function

--- lib/jvc.ex
+++ lib/jvc.ex
@@ -15,4 +15,27 @@ defmodule Jvc do
   def hello do
     :world
   end
+  
+  def main(args) do
+    args
+    |> parse_args
+    |> process
+  end
+  
+  def process([]) do
+    IO.puts "No arguments given"
+  end
+  
+  def process(options) do
+    IO.puts "Hello #{options[:name]}"
+  end
+  
+  defp parse_args(args) do
+    {options, _, _} = OptionParser.parse(
+      args,
+      switches: [foo: :string]
+    )
+    
+    options
+  end
 end
